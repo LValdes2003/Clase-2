a35ec44ccdd0df6998ac6e03718c4b419c3be9ec
LValdes2003 <145686477+LValdes2003@users.noreply.github.com>
2023-10-05 10:26:54 +0000

    guarda

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
+// This code is used to add two numbers and print the result.
+// It uses the add function to add two numbers and print the result.
+// It also uses the main function to print the result.
+int main() { // main function
+    int a = 5; // variable a
+    int b = 3; // variable b
+    int c = add(a, b); // variable c
+    printf("Hola Mundo!\n"); // print Hola Mundo!
+    printf("a = %d\n", a); // print a
+    printf("b = %d\n", b); // print b
+    printf("c = %d\n", c); // print c
+    return 0; // return 0
+}
 
-int main() {
-    printf("Hola Mundo!");
-    return 0;
+int add(int a, int b) { // add function
+    return a + b; // return a + b
+}
+
+int main() { // main function
+    int a = 5; // variable a
+    int b = 3; // variable b
+    int c = add(a, b); // variable c
+    printf("Hola Mundo!\n"); // print Hola Mundo!
+    printf("a = %d\n", a); // print a
+    printf("b = %d\n", b); // print b
+    printf("c = %d\n", c); // print c
+    return 0; // return 0
+}
+
+int add(int a, int b) { // add function
+    return a + b; // return a + b
 }
